9c0b15f510afde32013041a995c967dbd3d9c31a
Zeynep Şengül <zeynepsengul1650@gmail.com>
2023-03-06 15:02:49 +0300

    MusteriManager işlemleri yazıldı.

---
+++ ConsoleUI/MusteriManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    public class MusteriManager
+    { 
+        public void Add(Musteri musteri)
+        {
+            Console.WriteLine("Musteri Eklendi"+musteri.Ad);
+        }
+        public void Delete(Musteri musteri)
+        {
+            Console.WriteLine("Musteri Silindi" + musteri.Ad);
+        }
+        public void Update(Musteri musteri)
+        {
+            Console.WriteLine("Musteri Güncellendi" + musteri.Ad);
+        }
+        public void Listele(Musteri[] musteriler)
+        {
+            foreach (var musteri in musteriler)
+            {
+                Console.WriteLine(musteri.Ad);
+            }
+        }
+    }
+}
